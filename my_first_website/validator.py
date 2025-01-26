28583e92744b7780949dbe62bf99ad477620be47
Alina Makarovskaya <makarovskaya1307@gmail.com>
2025-01-27 01:35:20 +0300

    Update validator

--- my_first_website/validator.py
+++ my_first_website/validator.py
@@ -1,9 +1,9 @@
 def validate_user(user_data):
     errors = {}
+    if not user_data['name']:
+        errors['name'] = "Name is required"
     if not user_data['email']:
         errors['email'] = "Email is required"
     if not user_data['password']:
         errors['password'] = "Password is required"
-    if user_data['password'] != user_data['passwordConfirmation']:
-        errors['passwordConfirmation'] = "⚠️Passwords do not match"
     return errors
